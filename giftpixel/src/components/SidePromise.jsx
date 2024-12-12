3ba6afec23daeec32b2e98f125b9878320c9a6d9
yungflashh <streaktv8@gmail.com>
2024-12-12 15:38:22 +0100

    added the promise side bad nav

---
+++ giftpixel/src/components/SidePromise.jsx
@@ -0,0 +1,15 @@
+import FormInput from "../pages/SignInPage/FormInput"
+
+const SidePromise
+ = () => {
+  return (
+    <div>
+            <div className="Promise-select">
+                <FormInput type={"text"} label={"Promise Title"}/>
+            </div>
+
+    </div>
+  )
+}
+
+export default SidePromise;
