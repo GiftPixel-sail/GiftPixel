270ef41ee4796b15fe88d61235d58a777fe5b44e
Alagham <hamzahalagbe27@gmail.com>
2024-12-12 15:34:30 +0100

    added Propscard

--- giftpixel/src/components/PropsCard.jsx
+++ giftpixel/src/components/PropsCard.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Button from "../components/Button"
+import { PiShareFill } from "react-icons/pi";
+import "../../src/styles/PropsCard.css"
 
-const PropsCard = () => {
+const PropsCard = ({title, desc}) => {
   return (
-    <div>PropsCard</div>
+
+    <div>
+        <div>
+            <h2>{title}</h2>
+            <div>
+                <p>{desc}</p>
+                <Button label="Share" <PiShareFill />/>
+            </div>
+        </div>
+        <Button label="+ Add To List"/>
+    </div>
   )
 }
 
