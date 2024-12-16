3f68414528e008083f51f077a4f8024518048fba
kirahshub1 <oyinbabe1999@gmail.com>
2024-12-16 01:13:22 -0800

    ForgetPassword first commit

--- giftpixel/src/App.jsx
+++ giftpixel/src/App.jsx
@@ -4,6 +4,7 @@ import AboutPage from "./pages/AboutPage/AboutPage"
 import SignInPage from "./pages/SignInPage/SignInPage"
 import SignupPage from "./pages/SignupPage/SignupPage"
 import EmailVerificationPage from "./pages/EmailVerificationPage/EmailVerificationPage"
+import ForgetPassword from "./pages/ForgetPassword/ForgetPassword"
 
 function App() {
  
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element ={<Homepage/>} />
           <Route path="/aboutUs" element ={<AboutPage/>} />
           <Route path="/signIn" element ={<SignInPage/>} />
+          <Route path="/forgetPassword" element = {<ForgetPassword/>} />
           <Route path="/signUp" element ={<SignupPage/>} />
           <Route path="/emailVerificationPage" element ={<EmailVerificationPage/>} />
         </Routes>
