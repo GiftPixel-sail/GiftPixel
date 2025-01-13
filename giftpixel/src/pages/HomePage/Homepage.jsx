935ff3d04ca31c89e45a0c97e29c7b64b268a5d2
Declanny <chisomhenryg@gmail.com>
2025-01-13 17:02:27 +0100

    landing page

--- giftpixel/src/pages/HomePage/Homepage.jsx
+++ giftpixel/src/pages/HomePage/Homepage.jsx
@@ -1,14 +1,21 @@
-import { Link } from "react-router-dom"
-
-
+import Hero from "../LandingPage/Hero";
+import StepsSection from "../LandingPage/StepsSection";
+import FeaturesSection from "../LandingPage/FeaturesSection";
+import PromiseCardSection from "../LandingPage/PromiseCardSection";
+import AboutSection from "../LandingPage/AboutSection";
+import Testimonials from "../LandingPage/Testimonials";
 
 const Homepage = () => {
   return (
     <div>
-      <Link to={"/signUp"}>Sign up</Link>
-      <Link to={"/signIn"}>Sign In</Link> | <Link to="/forgotPassword">Forgot Password</Link>
+      <Hero />
+      <StepsSection />
+      <FeaturesSection />
+      <PromiseCardSection />
+      <AboutSection />
+      <Testimonials />
     </div>
   );
-}
+};
 
-export default Homepage
+export default Homepage;
