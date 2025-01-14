cf9d9bc2fe2bc99a4936b379a9c488634fe2fb18
yungflashh <streaktv8@gmail.com>
2025-01-14 05:44:19 +0100

    added the landing page

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -1,15 +1,21 @@
-import { Link } from "react-router-dom"
-import "../../styles/HomePage.css"
-
-
+import Hero from "../LandingPage/Hero";
+import StepsSection from "../LandingPage/StepsSection";
+import FeaturesSection from "../LandingPage/FeaturesSection";
+import PromiseCardSection from "../LandingPage/PromiseCardSection";
+import AboutSection from "../LandingPage/AboutSection";
+import Testimonials from "../LandingPage/Testimonials";
 
 const Homepage = () => {
   return (
-    <div className="nav-bar">
-      <Link to={"/signUp"}>Sign up</Link>
-      <Link to={"/signIn"}>Sign In</Link> | <Link to="/forgotPassword">Forgot Password</Link>
+    <div>
+      <Hero />
+      <StepsSection />
+      <FeaturesSection />
+      <PromiseCardSection />
+      <AboutSection />
+      <Testimonials />
     </div>
   );
-}
+};
 
-export default Homepage
+export default Homepage;
